91211cdce270cf4256dc1665970babf37b770bd5
shexweeknd <ramarosandratananantenaina@gmail.com>
2024-05-18 09:11:55 +0300

    effectué:     - ajout du fichier db_connect.php

---
+++ utils/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+  $server = "db5013327519.hosting-data.io";
+	$username = "dbu2518744";
+	$password = "Exxolia09062023xZk#";
+	$db = "dbs11176156";
+
+  $conn =  mysqli_connect($server, $username, $password, $db);
+?>
