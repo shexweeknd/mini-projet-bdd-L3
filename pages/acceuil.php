659396d83a51e829fe918ea106b05e73be2c91ab
SeshiriaC <herynirinacecilia@gmail.com>
2024-05-21 14:20:20 +0300

    fichiers concernés: acceuil.php, style.css(nouveau)    -ajout de l'interface du chat recommendations:    -revoir css si acceuil.php à réécrire comme auth.php

---
+++ pages/acceuil.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'acceuil</title>
+    <!-- <link rel="stylesheet"  href ="style.css"> -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+</head>
+<body>
+
+<div class="title">
+<h1>Yoyo Chat</h1>
+</div>
+<p class="bienvenu">Bienvenu @Nantenaina</p>
+<div class ="message-ji">
+
+<div class="mess1">
+        <div class ="message-so">
+            <div class="avatar">
+                <div class="cercle">
+                    <img src=" ../assets/img/cercle (1).png" alt="">
+                </div>
+                <div class="init"><h4>T</h4></div>
+            </div>
+            <div>
+                <div><h3>@Teggy</h3></div>
+                <div class="mess">
+                Lorem ipsum, dolor sit amet consectetur 
+                adipisicing elit. Commodi, molestias e
+                t at earum nemo dolorum praesentium quaerat
+                laudantium mollitia provident vel deserunt
+                cum est dolorem expedita quam. Dignissimos,
+                quia voluptate?
+                </div>
+            </div>
+        </div>
+        <div class="date_heure">01/05/2024 13:15</div>
+    </div>
+    <div class="mess1">
+        <div class ="message-so">
+            <div class="avatar">
+                <div class="cercle">
+                    <img src=" ../assets/img/cercle (1).png" alt="">
+                </div>
+                <div class="init"><h4>V</h4></div>
+            </div>
+            <div>
+                <div><h3>@Vinci</h3></div>
+                <div class="mess">
+                Lorem ipsum, dolor sit amet consectetur 
+                adipisicing elit. Commodi, molestias e
+                t at earum nemo dolorum praesentium quaerat
+                laudantium mollitia provident vel deserunt
+                cum est dolorem expedita quam. Dignissimos,
+                quia voluptate?
+                </div>
+            </div>
+        </div>
+        <div class="date_heure">01/05/2024 13:15</div>
+    </div>
+    <div class="mess1">
+        <div class ="message-so">
+            <div class="avatar">
+                <div class="cercle">
+                    <img src="../assets/img/cercle (1).png" alt="">
+                </div>
+                <div class="init"><h4>S</h4></div>
+            </div>
+            <div>
+                <div><h3>@Seshi</h3></div>
+                <div class="mess">
+                Lorem ipsum, dolor sit amet consectetur 
+                adipisicing elit. Commodi, molestias e
+                t at earum nemo dolorum praesentium quaerat
+                laudantium mollitia provident vel deserunt
+                cum est dolorem expedita quam. Dignissimos,
+                quia voluptate?
+                </div>
+            </div>
+        </div>
+        <div class="date_heure">01/05/2024 13:15</div>
+    </div>
+    
+</div>    
+<div class="message-ch">
+    <div>
+        <form action="">
+             <input type="text" placeholder="Message">
+         </form>
+    </div>
+    <div class="bouton">
+        <div class="cerc"><img src="../assets/img/cercle (2).png" alt=""></div>
+        <div class="env"><img src="../assets/img/envoyer.png" alt=""></div>
+    </div>
+</div>
+</body>
+</html>
