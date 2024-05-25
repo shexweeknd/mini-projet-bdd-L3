77c786ea8aaeb5e643eb5aa4a3edf48662fc7b28
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-05-25 08:06:31 +0300

    effectue:     - ajout des elements message_left.php et message_right.php pour gerer l'affichage des messages proprietaires et autres messages todo:     - test avec la base de donnee locale

---
+++ elements/message_left.php
@@ -0,0 +1,22 @@
+<div class="mess1">
+        <div class ="message-so">
+            <div class="avatar">
+                <div class="cercle">
+                    <img src=" ../assets/img/cercle (1).png" alt="">
+                </div>
+                <div class="init"><h4>T</h4></div>
+            </div>
+            <div>
+                <div><h3>@Teggy</h3></div>
+                <div class="mess">
+                Lorem ipsum, dolor sit amet consectetur 
+                adipisicing elit. Commodi, molestias e
+                t at earum nemo dolorum praesentium quaerat
+                laudantium mollitia provident vel deserunt
+                cum est dolorem expedita quam. Dignissimos,
+                quia voluptate?
+                </div>
+            </div>
+        </div>
+        <div class="date_heure">01/05/2024 13:15</div>
+    </div>
--- pages/acceuil.php
+++ pages/acceuil.php
@@ -13,31 +13,10 @@
 <div class="title">
 <h1>Yoyo Chat</h1>
 </div>
-<p class="bienvenu">Bienvenu @Nantenaina</p>
+<p class="bienvenu">Bienvenu <?php echo $_SESSION['username']?></p>
 <div class ="message-ji">
 
-<div class="mess1">
-        <div class ="message-so">
-            <div class="avatar">
-                <div class="cercle">
-                    <img src=" ../assets/img/cercle (1).png" alt="">
-                </div>
-                <div class="init"><h4>T</h4></div>
-            </div>
-            <div>
-                <div><h3>@Teggy</h3></div>
-                <div class="mess">
-                Lorem ipsum, dolor sit amet consectetur 
-                adipisicing elit. Commodi, molestias e
-                t at earum nemo dolorum praesentium quaerat
-                laudantium mollitia provident vel deserunt
-                cum est dolorem expedita quam. Dignissimos,
-                quia voluptate?
-                </div>
-            </div>
-        </div>
-        <div class="date_heure">01/05/2024 13:15</div>
-    </div>
+
     <div class="mess1">
         <div class ="message-so">
             <div class="avatar">
